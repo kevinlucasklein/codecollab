4c47af1455259dab141740306c2280e019b6f808
Kevin Klein <kevinlucasklein@gmail.com>
2026-05-25 00:13:16 -0600

    fix: correctly wrap RootLayout children with AuthProvider

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body>{children}</body>
+      <body>
+        <AuthProvider>{children}</AuthProvider>
+      </body>
     </html>
   );
 }
